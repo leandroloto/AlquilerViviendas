3200f94ada38d822d45a8f7ddd7f9cc0fb088a00
Emanuel <matias@192.168.1.86>
2018-12-10 11:12:27 -0300

    Todas las clases Creadas

--- src/alquilerviviendas/Viviendas.java
+++ src/alquilerviviendas/Viviendas.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package alquilerviviendas;
-
+import java.math.BigDecimal;
 /**
  *
  * @author Leandro
  */
 public class Viviendas {
     
+    protected int Identificador;
+    protected float MetrosCuadrados;
+    protected BigDecimal PrecioMensual;
+    protected Inquilino Inquilinos;
+    
+    /**
+     * 
+     * @param Identificador
+     * @param MetrosCuadrados
+     * @param PrecioMensual 
+     */
+    public Viviendas(int Identificador, float MetrosCuadrados, BigDecimal PrecioMensual,Inquilino Inquilinos){
+        this.Identificador=Identificador;
+        this.MetrosCuadrados=MetrosCuadrados;
+        this.PrecioMensual=PrecioMensual;
+        this.Inquilinos=Inquilinos;
+    }
 }
